6f57918f26b0e1c8590d78dca62f78a30d4c4233
leochuan <ruochuan.leo@gmail.com>
2017-11-07 14:03:12 +0800

    finish chapter four

--- pratise.scm
+++ pratise.scm
@@ -84,3 +84,124 @@
       ((null? lat) (quote ()))
       ((eq? old (car lat)) (cons new (multisubst new old (cdr lat))))
       (else (cons (car lat) (multisubst new old (cdr lat)))))))
+
+(define add1 
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define o+
+  (lambda (n m)
+    (cond
+      ((not (zero? n)) (o+ (sub1 n) (add1 m)))
+      (else m))))
+
+(define o-
+  (lambda (n m)
+    (cond 
+      ((zero? m) n)
+      (else (o- (sub1 n) (sub1 m))))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (o+ (car tup) (addtup (cdr tup)))))))
+
+(define o*
+  (lambda (n m)
+    (cond
+      ((zero? m) 0)
+      (else (o+ n (o* n (sub1 m)))))))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond 
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
+      (else (cons (o+ (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define o>
+  (lambda (m n)
+    (cond 
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else (o> (sub1 m) (sub1 n))))))
+
+(define o<
+  (lambda (m n)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else (o< (sub1 m) (sub1 n))))))
+
+(define o=
+  (lambda (m n)
+    (cond
+      ((o< m n) #f)
+      ((o> m n) #f)
+      (else #t))))
+
+(define power
+  (lambda (m n)
+    (cond
+      ((zero? n) 1)
+      (else (o* m (power m (sub1 n)))))))
+
+(define o/
+  (lambda (m n)
+    (cond
+      ((o< m n) 0)
+      (else (add1 (o/ (o- m n) n))))))
+
+(define len
+  (lambda (lat)
+    (cond
+      ((null? lat) 0)
+      (else (add1 (len (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((one? n) (cdr lat))
+      (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((number? (car lat)) (no-nums (cdr lat)))
+      (else (cons (car lat) (no-nums (cdr lat)))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
+      (else (all-nums (cdr lat))))))
+
+(define eqan?
+  (lambda (q1 q2)
+    (cond
+      ((number? q1) (and (number? q2) (o= q1 q2)))
+      (else (and (not (number? q2)) (eq? q1 q2))))))
+
+(define occur
+  (lambda (n lat)
+    (cond
+      ((null? lat) 0)
+      ((eqan? n (car lat)) (add1 (occur n (cdr lat))))
+      (else (occur n (cdr lat))))))
+
+(define one?
+  (lambda (n)
+    (= n 1)))
